b49e1595b07c2e891dda9a0e6da6ddd854fc63be
Haroun Butt <haroun@outlook.com>
2024-04-26 23:34:33 +0100

    Add mini files

--- init.lua
+++ init.lua
@@ -822,6 +822,8 @@ require('lazy').setup({
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
+      require('mini.files').setup {options = {use_as_default_explorer = true}}
+
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
